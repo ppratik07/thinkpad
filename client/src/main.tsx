9a1bdec45d66fb5653fa453ab20925b0eeb6885d
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-03-17 18:31:01 +0530

    Updating the main page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { Dashboard } from './pages/Dashboard'
+import { History } from './pages/History'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/history" element={<History/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
